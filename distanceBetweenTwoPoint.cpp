3a8137613b241acafef55e9f31dfaa5570171c97
Safin Bhuiyan <112971550+SafinBhuiyan@users.noreply.github.com>
2023-01-20 22:12:44 +0600

    Create distanceBetweenTwoPoint.cpp

---
+++ distanceBetweenTwoPoint.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+        double x , x1 , x2 , y , y1 , y2;
+        cin >> x1 >> y1;
+        cin >> x2 >> y2;
+
+        x = (x2-x1)*(x2-x1);
+        y = (y2-y1)*(y2-y1);
+
+        double distance;
+        distance = sqrt(x+y);
+
+        cout << fixed << setprecision(4) << distance << endl;
+
+	return 0;
+}
